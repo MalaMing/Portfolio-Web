3256d16773543a8f4ba858528469414dbb9efefc
Malaming <minggggggg@gmail.com>
2025-02-24 02:33:01 +0700

    Enhance homepage layout with updated profile section and add TextButton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,28 @@
 
+import TextButton from "@/components/TextButton";
 import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="">
-
-      <div>
-        <div>
-          Hi, I'm <span>Phunyisa</span>
+    <div className="bg-black-theme-black flex flex-col items-center justify-center">
+      <div className="flex flex-col justify-center items-center ">
+        <div className="text-black-theme-white text-5xl font-bold">
+          Hi, I'm <span className="bg-gradient-to-r from-orange-300 via-rose-300 to-indigo-400 inline-block text-transparent bg-clip-text">Phunyisa</span>
         </div>
-        <div className="flex flex-col ">
+        <div className="flex flex-col justify-center items-center  ">
           <Image
             src="/images/profile.png"
-            width={500}
-            height={500}
+            width={700}
+            height={700}
             alt=""
           />
-          <p>I’m a computer science student at Kasetsart University with a passion for UI design and front-end development.</p>
+          <p className="text-2xl font-medium text-white opacity-65">I’m a computer science student at Kasetsart University with a passion for UI design and front-end development.</p>
         </div>
         <div>
           <div>let's get intouch</div>
           <div>
-            <button> Download Resume </button>
-            <button> Download CV </button>
+            <TextButton text="Download Resume"/>
+            <TextButton text="DownloadCV"/>
           </div>
         </div>
       </div>
---
+++ src/components/TextButton.tsx
@@ -0,0 +1,6 @@
+export default function TextButton({text}:{text:string}){
+    return (
+        <button className="text-black-theme-white text-lg border-[1px] rounded-[100px]  pb-[10px] pt-[10px] pl-[20px] pr-[20px]">{text}</button>
+    )   
+
+}
