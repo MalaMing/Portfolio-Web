546bd4166dc3505d9a9ab686d1acfad6546a146d
Malaming <minggggggg@gmail.com>
2025-02-24 01:12:20 +0700

    Add profile image and enhance homepage layout with personal introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 
-export default function Home() {
-  return (
-    
-    <div className="font-bold">
+import Image from "next/image";
 
+export default function Page() {
+  return (
+    <div className="">
+
+      <div>
+        <div>
+          Hi, I'm <span>Phunyisa</span>
+        </div>
+        <div className="flex flex-col ">
+          <Image
+            src="/images/profile.png"
+            width={500}
+            height={500}
+            alt=""
+          />
+          <p>I’m a computer science student at Kasetsart University with a passion for UI design and front-end development.</p>
+        </div>
+        <div>
+          <div>let's get intouch</div>
+          <div>
+            <button> Download Resume </button>
+            <button> Download CV </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
