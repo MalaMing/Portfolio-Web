238412918dfadabbd898b2c99c1cde0b44de51d1
Malaming <minggggggg@gmail.com>
2025-02-24 12:39:27 +0700

    re-design button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div className="bg-black-theme-black flex flex-col items-center justify-center">
+      
       <div className="flex flex-col justify-center items-center mt-[110px] mb-[110px] ml-[390px] mr-[390px] ">
         <div className="text-black-theme-white text-5xl font-bold ">
           Hi, I'm <span className="bg-gradient-to-r from-orange-300 via-rose-300 to-indigo-400 inline-block text-transparent bg-clip-text">Phunyisa</span>
@@ -21,11 +22,15 @@ export default function Page() {
         </div>
         <div className="flex flex-col justify-center items-center mt-[55px]">
           <NeonText text="let's get intouch"/>
-          <div className="flex flex-row justify-center gap-[20px] mt-[55px]"> 
+          <div className="flex flex-row justify-center gap-[20px] mt-[40px]"> 
             <TextButton text="Download Resume"/>
             <TextButton text="DownloadCV"/>
           </div>
         </div>
+      </div>
+
+      <div>
+
       </div>
     </div>
   );
